c0f645b581325a60f6c877d74c97ce13b46fdb3b
piyush <22066608@studentmail.ul.ie>
2023-02-26 23:17:37 +0000

    Display Framework Update

---
+++ Interceptor_Wether/Classes/Framework.cs
@@ -0,0 +1,23 @@
+﻿using Interceptor_Wether.Classes;
+using Interceptor_Wether.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interceptor_Wether.Classes
+{
+    class Framework
+    {
+
+        public void eventTrigger(double temprature, double humidity, double pressure)
+        {
+            WeatherDataContext context = new WeatherDataContext(temprature, humidity, pressure);
+            WeatherDIspatcher dispatcher = new WeatherDIspatcher();
+            IWeatherInterceptor interceptor = new WeatherInterceptor();
+            dispatcher.RegisterWeatherDataUpdateInterceptor(interceptor);
+            dispatcher.DispatchWeatherDataUpdate(context);
+        }
+    }
+}
---
+++ Interceptor_Wether/Classes/WeatherDataContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interceptor_Wether.Classes
+{
+    class WeatherDataContext
+    {
+        private double tempertature;
+        private double humidity;
+        private double pressure;
+
+
+        private WeatherStation station = new WeatherStation();
+        private Display display = new Display();
+
+        public WeatherDataContext(double tempertature, double humidity, double pressure )
+        {
+            this.tempertature = tempertature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+        }
+
+        public double getTemprature()
+        {
+            return tempertature;
+        }
+        public double getPressure()
+        {
+            return pressure;
+        }
+        public double getHumidity()
+        {
+            return humidity;
+        }
+
+        public WeatherStation getWeatherStationReference()
+        {
+            return station;
+        }
+
+        public Display getDisplayReference()
+        {
+            return display;
+        }
+    }
+}
